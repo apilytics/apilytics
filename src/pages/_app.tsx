8db67dc818f063f6d97070aefbd1bd8d459460df
Eero Ruohola <eero@ruohola.dev>
2021-12-13 21:57:25 +0200

    Disable Plausible Analytics in non-production environments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps } from 'next/app';
 import { FRONTEND_DOMAIN } from 'utils';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <PlausibleProvider domain={FRONTEND_DOMAIN}>
+  <PlausibleProvider domain={FRONTEND_DOMAIN} enabled={process.env.VERCEL_ENV === 'production'}>
     <Component {...pageProps} />
   </PlausibleProvider>
 );
