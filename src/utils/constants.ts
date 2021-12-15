5f5178d66bb05b77e267c254bb0fd3f24136f333
Markus Blomqvist <blomqvist_markus@hotmail.com>
2021-12-16 00:08:26 +0200

    Simplify description text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const TITLE = 'Simple analytics for your APIs';
 
 export const DESCRIPTION =
-  'Apilytics is a simple service that allows you to monitor & visualize operational, performance and security metrics from your APIs!';
+  'Apilytics allows you to monitor & visualize operational, performance and security metrics from your APIs.';
 
 export const FRONTEND_URL = 'https://www.apilytics.io';
 export const FRONTEND_DOMAIN = 'apilytics.io';
