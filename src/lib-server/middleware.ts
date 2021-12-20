795f53c2498be7060292c4917cda8ecffd78aa38
Eero Ruohola <eero@ruohola.dev>
2021-12-20 20:54:54 +0200

    Fix unauthenticated access to auth requiring APIs resulting in 500 error
    Reason was that the `withAuthRequired` middleware could be (and was)
    placed outside the `makeMethodsHandler` factory function, and thus the
    API handler that `makeMethodsHandler` returns could not catch the
    `UnauthorizedApiError` that `withAuthRequired` would throw.

--- src/lib-server/middleware.ts
+++ src/lib-server/middleware.ts
@@ -1,9 +1,14 @@
-import { getSessionUser } from 'lib-server/apiHelpers';
+import { safeGetSessionUser } from 'lib-server/apiHelpers';
+import { sendUnauthorized } from 'lib-server/responses';
 import type { ApiHandler } from 'lib-server/types';
 
 export const withAuthRequired = <T extends ApiHandler>(handler: T) => {
   return async (req: Parameters<T>[0], res: Parameters<T>[1]): Promise<void> => {
-    await getSessionUser(req); // Throws an error if not authorized.
-    await handler(req, res);
+    const user = await safeGetSessionUser(req);
+    if (!user) {
+      sendUnauthorized(res);
+    } else {
+      await handler(req, res);
+    }
   };
 };
