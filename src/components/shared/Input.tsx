c1bee5afd63a0386935b91e4f31d7810da5fcf61
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-01-08 17:16:06 +0200

    Fix custom input class names

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -5,14 +6,14 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   helperText?: string;
 }
 
-export const Input: React.FC<Props> = ({ label, helperText, ...props }) => (
+export const Input: React.FC<Props> = ({ label, helperText, className, ...props }) => (
   <div className="form-control">
     <label className="label">
       <span className="label-text">
         {label} {props.required && <span className="text-error">*</span>}
       </span>
     </label>
-    <input className={'input'} {...props} />
+    <input className={clsx('input', className)} {...props} />
     {helperText && (
       <label className="label">
         <span className="label-text-alt">{helperText}</span>
