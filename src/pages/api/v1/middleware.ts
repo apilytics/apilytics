fb4376c4346c3dba5209d1482d0cc00cf8435b9c
Eero Ruohola <eero@ruohola.dev>
2022-01-08 11:12:24 +0200

    Add `statusCode` to `Metric` and to the middleware API
    We can add rendering of this in the dashboard at a later time.

--- src/pages/api/v1/middleware.ts
+++ src/pages/api/v1/middleware.ts
@@ -10,7 +10,7 @@ import {
 import prisma from 'prismaClient';
 import type { ApiHandler } from 'types';
 
-type PostBody = Pick<Metric, 'path' | 'method' | 'timeMillis'>;
+type PostBody = Pick<Metric, 'path' | 'method' | 'statusCode' | 'timeMillis'>;
 
 const handlePost: ApiHandler = async (req, res) => {
   const apiKey = req.headers['x-api-key'];
@@ -25,14 +25,14 @@ const handlePost: ApiHandler = async (req, res) => {
     return;
   }
 
-  const { path, method, timeMillis } = req.body as PostBody;
-  if ([path, method, timeMillis].some((field) => field === undefined)) {
+  const { path, method, statusCode, timeMillis } = req.body as PostBody;
+  if ([path, method, statusCode, timeMillis].some((field) => field === undefined)) {
     sendInvalidInput(res);
     return;
   }
 
   await prisma.metric.create({
-    data: { originId: origin.id, path, method, timeMillis },
+    data: { originId: origin.id, path, method, statusCode, timeMillis },
   });
 
   sendOk(res);
