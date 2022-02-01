3efef4d762be238d221f34653e8428995c784dbe
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-02-01 18:11:14 +0200

    Update "others" option  in setup section
    Show info about the other frameworks
    only after selecting a framework.

--- src/components/shared/FrameworkSelection.tsx
+++ src/components/shared/FrameworkSelection.tsx
@@ -3,30 +3,42 @@ import type { ChangeEvent } from 'react';
 
 import { Select } from 'components/shared/Select';
 import { usePlausible } from 'hooks/usePlausible';
+import { staticRoutes } from 'utils/router';
+import Link from 'next/link';
 
 const FRAMEWORKS = ['Ruby on Rails', 'Laravel', '.NET', 'Spring', 'Flask', 'Other'];
 
 export const FrameworkSelection: React.FC = () => {
   const [value, setValue] = useState('');
+  const [textVisible, setTextVisible] = useState(false);
   const plausible = usePlausible();
 
   const handleChange = ({ target: { value } }: ChangeEvent<HTMLSelectElement>): void => {
     setValue(value);
     plausible('setup-framework-select', { value });
+
+    if (!textVisible) {
+      setTextVisible(true);
+    }
   };
 
   return (
     <>
-      <label className="label">
-        <span className="label-text">Which framework are you using?</span>
-      </label>
-      <Select name="framework" value={value} onChange={handleChange} required>
+      <p>Which framework are you using?</p>
+      <Select name="framework" value={value} onChange={handleChange} required className="my-4">
         {FRAMEWORKS.map((val) => (
           <option value={val} key={val}>
             {val}
           </option>
         ))}
       </Select>
+      {textVisible && (
+        <p>
+          If your backend supports none of our open source middlewares, we have tools to help you
+          with creating your own middleware. See our <Link href={staticRoutes.byom}>docs</Link> for
+          more information.
+        </p>
+      )}
     </>
   );
 };
