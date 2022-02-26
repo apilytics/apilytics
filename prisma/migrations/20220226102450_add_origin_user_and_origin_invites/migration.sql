5f5f43427d83c01227acad33bc9d253e461d1be0
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-02-26 12:40:50 +0200

    Add data models for origin users and origin invites

---
+++ prisma/migrations/20220226102450_add_origin_user_and_origin_invites/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "origin_users" (
+    "id" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "origin_id" UUID NOT NULL,
+
+    CONSTRAINT "origin_users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "origin_invites" (
+    "id" UUID NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "origin_id" UUID NOT NULL,
+
+    CONSTRAINT "origin_invites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "origin_invites_email_key" ON "origin_invites"("email");
+
+-- AddForeignKey
+ALTER TABLE "origin_users" ADD CONSTRAINT "origin_users_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "origin_users" ADD CONSTRAINT "origin_users_origin_id_fkey" FOREIGN KEY ("origin_id") REFERENCES "origins"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "origin_invites" ADD CONSTRAINT "origin_invites_origin_id_fkey" FOREIGN KEY ("origin_id") REFERENCES "origins"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO "origin_users" (
+  "id",
+  "role",
+  "updated_at",
+  "user_id",
+  "origin_id"
+) SELECT
+    uuid_generate_v4(),
+    'owner',
+    NOW(),
+    "user_id",
+    "id"
+  FROM "origins";
+
+-- DropForeignKey
+ALTER TABLE "origins" DROP CONSTRAINT "origins_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "origins" DROP COLUMN "user_id";
