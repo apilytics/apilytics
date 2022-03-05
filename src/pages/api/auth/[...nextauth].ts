6e071b19086f75c341893ab451bdadcad95991d8
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-03-05 10:54:20 +0200

    Log auth emails to console in dev environment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 
+import { sendVerificationRequest } from 'lib-server/apiHelpers';
 import prisma from 'prisma/client';
 import { withApilytics } from 'utils/apilytics';
 import { staticRoutes } from 'utils/router';
@@ -28,6 +29,7 @@ const handler = NextAuth({
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
     }),
   ],
   secret: process.env.SECRET_KEY,
