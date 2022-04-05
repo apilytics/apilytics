4e9745a3e206cb0129624a6a3c328a9b3e9d103c
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-04-05 23:20:28 +0300

    Bump sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,7 +11,7 @@ import {
 } from 'utils/router';
 
 // This should be manually updated whenever we want Google to re-index the sitemap.
-const MODIFIED = '2022-03-22';
+const MODIFIED = '2022-04-05';
 
 type ChangeFreq = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
 
