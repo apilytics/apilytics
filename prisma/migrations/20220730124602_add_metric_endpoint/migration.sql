d9d055c25824632e5c99d648a34bfa66714aa2e2
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-08-16 23:23:10 +0300

    Attempt to fix `add_metric_endpoint` migration #2

--- prisma/migrations/20220730124602_add_metric_endpoint/migration.sql
+++ prisma/migrations/20220730124602_add_metric_endpoint/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "metrics" ADD COLUMN     "endpoint" TEXT NOT NULL DEFAULT E'';
+ALTER TABLE "metrics" ADD COLUMN     "endpoint" TEXT NOT NULL;
 
 UPDATE metrics
 
@@ -7,6 +7,7 @@ SET endpoint = (
     SELECT CASE WHEN route IS NULL THEN metrics.path ELSE route END
     FROM dynamic_routes
     WHERE dynamic_routes.id = metrics.dynamic_route_id
+        AND metrics.path IS NOT NULL
 );
 
 -- DropIndex
