52e061fbadd6e29da84f4d1a523f29a3cc775366
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-08-16 23:02:19 +0300

    Add `endpoint` column to the metrics index

--- prisma/migrations/20220730124602_add_metric_endpoint/migration.sql
+++ prisma/migrations/20220730124602_add_metric_endpoint/migration.sql
@@ -1,2 +1,8 @@
 -- AlterTable
 ALTER TABLE "metrics" ADD COLUMN     "endpoint" TEXT NOT NULL;
+
+-- DropIndex
+DROP INDEX "metrics_origin_id_path_method_status_code_browser_os_device_idx";
+
+-- CreateIndex
+CREATE INDEX "metrics_origin_id_path_endpoint_method_status_code_browser__idx" ON "metrics"("origin_id", "path", "endpoint", "method", "status_code", "browser", "os", "device", "country", "region", "city", "dynamic_route_id", "excluded_route_id", "created_at");
